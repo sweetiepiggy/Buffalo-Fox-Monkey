379c5a5882ec71c9270c505e532337c5061269fe
Sweetie Piggy <sweetiepiggyapps@gmail.com>
2012-09-05 20:03:18 +0800

    created basic code structure

--- src/com/sweetiepiggy/buffalofoxmonkey/AboutActivity.java
+++ src/com/sweetiepiggy/buffalofoxmonkey/AboutActivity.java
@@ -20,40 +20,15 @@
 package com.sweetiepiggy.buffalofoxmonkey;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
-public class BuffaloFoxMonkeyActivity extends Activity
-{
+public class AboutActivity extends Activity {
 
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.options_menu, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.about:
-			Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
-			startActivity(intent);
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
+		setContentView(R.layout.about);
 	}
 }
 
